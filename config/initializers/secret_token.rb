e4e3c76033bd59e5ca9858c7097ea39ba1601080
maverick <maverick@shelby.(none)>
2014-03-14 15:55:39 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deml::Application.config.secret_key_base = 'cea6592a905d682574bfec4f2b601f970914c0b6959991dbc8661dee777b04c08dfcfd56815b79da47813f10624100f04dc58857b96d11ef64fc0f396ebcf33e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Deml::Application.config.session_store :cookie_store, key: '_deml_session'
